3515fbddf6f0e9e2e1f4907125c1aa2b12c83dcf
Joel Tewhare <joeltewhare@gmail.com>
2026-03-04 19:27:47 +1300

    Add Navbar component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from './Navbar'
 
 export default function App() {
   return (
     <>
+      <Navbar />
       <main>
         <Outlet />
       </main>
